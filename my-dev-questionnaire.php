4980b2558f88e1dc33be26493249709fe2f6d30f
Mohamed Youssef <egsyntax@gmail.com>
2022-06-12 06:32:37 +0200

    Creating Database Table & solving some minor bugs

--- my-dev-questionnaire.php
+++ my-dev-questionnaire.php
@@ -18,14 +18,21 @@
 
 if ( !defined( 'ABSPATH' ) ) exit();
 
+/** Menu Lists */
+
 function my_dev_register_ques_main_menu() {
     add_menu_page( __( 'MY Questionnaire', 'my-dev-questionnaire' ), __( 'MY Questionnaire', 'my-dev-questionnaire' ), 'manage_options', 'my_questionnaire_Dashboard', 'my_dev_main_menu_page', 'dashicons-chart-bar', 100 );
 	add_submenu_page( 'my_questionnaire_Dashboard', __( 'About', 'my-dev-questionnaire' ), __( 'About', 'my-dev-questionnaire' ), 'manage_options', 'about', 'my_dev_register_about_ques_page', 1 );
 }
 
+/**  */
+
 function my_dev_main_menu_page() {
 
-    echo "Hello World";
+    echo "Hello World <br />";
+
+
+    global $wpdb;
 
 }
 
@@ -42,13 +49,14 @@ function my_dev_activate_ques_plugin() {
 
     global $wpdb;
 
+    // Check if page exsits
     $page_title = wp_strip_all_tags( 'My Questionnaire' );
 
-    $var_query = "SELECT COUNT(ID) FROM {$wpdb->posts} WHERE post_title = '{$page_title}'";
+    $check_page_exists_query = "SELECT COUNT(ID) FROM {$wpdb->posts} WHERE post_title = '{$page_title}'";
 
-    $is_created = $wpdb->get_var( $var_query );
+    $is_exists = $wpdb->get_var( $check_page_exists_query );
 
-    if ( $is_created == '0' ) {
+    if ( $is_exists == '0' ) {
 
         $my_dev_post = array(
             'post_author'       => get_current_user_id(),
@@ -62,7 +70,8 @@ function my_dev_activate_ques_plugin() {
         wp_insert_post( $my_dev_post, true );
     }
 
-    if ( $is_created > 0 ) {
+    // Update page to published
+    if ( $is_exists > 0 ) {
 
         $data_query = array(
             'order' => [ 'post_status' => 'publish', ],
@@ -71,6 +80,26 @@ function my_dev_activate_ques_plugin() {
 
         $wpdb->update( $wpdb->posts, $data_query['order'], $data_query['where'] );
 
+    }
+
+    // Check if Database Table exists
+    $table_name = $wpdb->prefix . "questionnaire";
+    
+    $database_table_check_query = "SHOW TABLES LIKE '{$table_name}'";
+
+    $database_table_is_exists = $wpdb->get_var( $database_table_check_query );
+
+    if ( $database_table_is_exists == null ) {
+        $create_database_table_query = "CREATE TABLE IF NOT EXISTS `{$table_name}` (
+            ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            name VARCHAR(64) NOT NULL,
+            national_id VARCHAR(14) NOT NULL,
+            mobile_no VARCHAR(11) NOT NULL,
+            signture VARCHAR(60) NOT NULL,
+            PRIMARY KEY (ID)
+        )";
+
+        $wpdb->query( $create_database_table_query );
     }
 
 }
@@ -104,7 +133,8 @@ function my_dev_uninstall_ques_plugin() {
 }
 
 
-// Register menu list
+/** MY-Dev */
+
 add_action( 'admin_menu', 'my_dev_register_ques_main_menu' );
 
 register_activation_hook( __FILE__, 'my_dev_activate_ques_plugin' );
